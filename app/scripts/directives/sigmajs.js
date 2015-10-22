31497ac440fd0f058fdacb60bc56a9001d479921
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-10-22 15:23:11 +0300

    References #3, radical changes to sigmajs directive.
    This allows to change course identifiers and graphs work just fine,
    although has its issues (comment mentiones all of those). It also seems
    that large graphs do not work so well with a lot of nodes and edges
    (unless browser has WebGL suport).

--- app/scripts/directives/sigmajs.js
+++ app/scripts/directives/sigmajs.js
@@ -10,12 +10,18 @@
 // Based on this Gist at https://gist.github.com/patrickfav/cd20939b383b9c284511
 // And example of neighbours from project page at http://sigmajs.org/
 
+// XXX This directive kills sigmajs instance every time $destroy is calles
+// It also recreates the instane for each chage to the data set
+// This is a bad approach but it seems to work just fine for the current need
+
 angular.module('emmaDashboardApp')
   .directive('sigmajs', function ($window) {
-    var containerId = 'sigmajs-container-' + Math.random() * 9999,
+    var s = null;
+    var containerId = 'sigmajs-container' + Math.random() * 9999,
         notKeptColor = '#eee',
         w = angular.element($window);
 
+    // Add a generally available method to sigmajs graph
     window.sigma.classes.graph.addMethod('neighbors', function(nodeId) {
       var k,
           neighbors = {},
@@ -35,19 +41,68 @@ angular.module('emmaDashboardApp')
         graph: '='
       },
       link: function postLink(scope, element) {
-        var s = new window.sigma({
-          container: containerId,
-          settings: {
-            defaultNodeColor: '#ec5148',
-            minNodeSize: 1,
-            maxNodeSize: 10,
-            minEdgeSize: 1,
-            maxEdgeSize: 2.5,
-            hideEdgesOnMove: true
-          }
-        });
 
         scope.$watch('graph', function () {
+
+          if ( !s ) {
+            s = new window.sigma({
+              renderers: [
+                {
+                  container: document.getElementById(containerId)
+                }
+              ],
+              settings: {
+                defaultNodeColor: '#ec5148',
+                minNodeSize: 1,
+                maxNodeSize: 10,
+                minEdgeSize: 1,
+                maxEdgeSize: 2.5,
+                hideEdgesOnMove: true
+              }
+            });
+
+            s.bind('clickNode', function(e) {
+                var nodeId = e.data.node.id,
+                    toKeep = s.graph.neighbors(nodeId);
+                toKeep[nodeId] = e.data.node;
+
+                s.graph.nodes().forEach(function(n) {
+                  if (toKeep[n.id]) {
+                    n.color = n.originalColor;
+                  } else {
+                    n.color = notKeptColor;
+                  }
+                });
+
+                s.graph.edges().forEach(function(e) {
+                  if (toKeep[e.source] && toKeep[e.target]) {
+                    e.color = e.originalColor;
+                  } else {
+                    e.color = notKeptColor;
+                  }
+                });
+
+                // Since the data has been modified, we need to
+                // call the refresh method to make the colors
+                // update effective.
+                s.refresh();
+              });
+
+              s.bind('clickStage', function() {
+                s.graph.nodes().forEach(function(n) {
+                  n.color = n.originalColor;
+                });
+
+                s.graph.edges().forEach(function(e) {
+                  e.color = e.originalColor;
+                });
+
+                // Same as in the previous event:
+                s.refresh();
+              });
+          }
+
+          s.render();
           s.graph.clear();
           s.graph.read(scope.graph);
 
@@ -61,60 +116,21 @@ angular.module('emmaDashboardApp')
             edge.originalColor = edge.color;
           });
 
-          // For some reason it needs two refreshes
-          s.refresh();
           s.refresh();
         });
 
         element.on('$destroy', function() {
-          //s.graph.clear();
+          s.kill();
+          s = null;
         });
 
-        s.bind('clickNode', function(e) {
-            var nodeId = e.data.node.id,
-                toKeep = s.graph.neighbors(nodeId);
-            toKeep[nodeId] = e.data.node;
-
-            s.graph.nodes().forEach(function(n) {
-              if (toKeep[n.id]) {
-                n.color = n.originalColor;
-              } else {
-                n.color = notKeptColor;
-              }
-            });
-
-            s.graph.edges().forEach(function(e) {
-              if (toKeep[e.source] && toKeep[e.target]) {
-                e.color = e.originalColor;
-              } else {
-                e.color = notKeptColor;
-              }
-            });
-
-            // Since the data has been modified, we need to
-            // call the refresh method to make the colors
-            // update effective.
+        w.bind('resize', function () {
+          var child = element.children()[0];
+          angular.element(child).css('height', w.height() - 100).css('width', w.width() - 30);
+          if ( s ) {
             s.refresh();
-          });
-
-          s.bind('clickStage', function() {
-            s.graph.nodes().forEach(function(n) {
-              n.color = n.originalColor;
-            });
-
-            s.graph.edges().forEach(function(e) {
-              e.color = e.originalColor;
-            });
-
-            // Same as in the previous event:
-            s.refresh();
-          });
-
-          w.bind('resize', function () {
-            var child = element.children()[0];
-            angular.element(child).css('height', w.height() - 100).css('width', w.width() - 30);
-            s.refresh();
-          });
+          }
+        });
       }
     };
   });

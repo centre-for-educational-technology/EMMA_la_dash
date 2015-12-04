5c377727587875b07c7926b89ddee77971cb90c9
GlebRed <dev.gleb.redko@gmail.com>
2015-12-04 17:57:00 +0200

    Student dashboard: handling data for student progress comparison

--- app/scripts/controllers/course.js
+++ app/scripts/controllers/course.js
@@ -365,7 +365,12 @@ angular.module('emmaDashboardApp')
         $scope.assignments = data.assignments;
         $scope.assignments_submitted = data.learning_content.materials.assignments_submitted;
 
-        $scope.students = data.students;
+        $scope.avg_units_visited_by_students = data.avg_units_visited_by_students;
+        $scope.avg_score_by_students = data.avg_score_by_students;
+        $scope.units_visited_by_me = data.units_visited_by_me;
+        $scope.avg_score_by_me = data.avg_score_by_me;
+
+
 
 
 
@@ -388,6 +393,22 @@ angular.module('emmaDashboardApp')
           $(".dd-nodrag").on("click", function(event) { // click event
             event.preventDefault();
             return false;
+          });
+
+          $('.easy-pie-chart.percentage').each(function(){
+            var $box = $(this).closest('.infobox');
+            var barColor = $(this).data('color') || (!$box.hasClass('infobox-dark') ? $box.css('color') : 'rgba(255,255,255,0.95)');
+            var trackColor = barColor === 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)' : '#E2E2E2';
+            var size = parseInt($(this).data('size')) || 50;
+            $(this).easyPieChart({
+              barColor: barColor,
+              trackColor: trackColor,
+              scaleColor: false,
+              lineCap: 'butt',
+              lineWidth: parseInt(size/10),
+              animate: /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase()) ? false : 1000,
+              size: size
+            });
           });
         });
 

2f5791ae2d3a72b81cc733e02968db001f876f2c
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-12-13 13:26:25 +0200

    Updated Gruntfile, make sure fontawsome fonts are copied into dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,6 +411,12 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: '.',
+          src: 'bower_components/font-awesome/fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
